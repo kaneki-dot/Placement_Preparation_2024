3e9721b29032259988922e6962181e546ede74d1
Rajat Dixit <73271760+kaneki-dot@users.noreply.github.com>
2023-07-18 14:29:21 +0530

    Update Rotate Image.cpp

--- Array 2 /Rotate Image.cpp
+++ Array 2 /Rotate Image.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-   void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int a = 0;
-        int b = n-1;
-        while(a<b){
-            for(int i=0;i<(b-a);++i){
-                swap(matrix[a][a+i], matrix[a+i][b]);
-                swap(matrix[a][a+i], matrix[b][b-i]);
-                swap(matrix[a][a+i], matrix[b-i][a]);
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                swap(matrix[i][j],matrix[j][i]);
             }
-            ++a;
-            --b;
         }
+        for(int i=0;i<n;i++)
+            reverse(matrix[i].begin(),matrix[i].end());
     }
 };
